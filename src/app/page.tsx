6c361781a2f00abb866aec08ecf4ffe0fe3a0e25
Ondrej Rohon <ondrej.rohon@gmail.com>
2023-02-14 15:11:16 +0100

    font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ["400", "700"] })
 
 export default function Home() {
   return (
-    <main className='text-red-500'>
+    <main className={`${poppins.className} bg-blue-50`}>
       home
     </main>
   )
